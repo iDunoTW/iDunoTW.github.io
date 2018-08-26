c7662075e02c0c740b9c4d7f8fb55d85d78c0f53
iDunoTW <42703942+iDunoTW@users.noreply.github.com>
2018-08-26 23:07:45 +0200

    Create test.user.js
    button test

---
+++ test.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         auto install test
+// @namespace    https://idunotw.github.io/
+// @version      0.1
+// @description  try to take over the world!
+// @author       iDuno
+// @match        *
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert("suc. installed");
+
+})();
